2cb7aa91751dfa2929b65ca9a0a3a06c17148faf
Helmsj14 <dillonhelms06@gmail.com>
2026-02-05 20:10:03 -0500

    first commit

---
+++ src/Assignnment2.java
@@ -0,0 +1,40 @@
+public class Assignnment2 {
+    public class Fraction {
+private int numerator;
+private int denominator;
+    
+public Fraction(int num, int denom){
+
+}
+
+    return new Fraction(newNum, newDenom);
+}
+public Fraction subtract(Fraction inFraction) {
+
+}
+public Fraction multiply(Fraction inFraction) {
+
+}
+public Fraction divide(Fraction inFraction) {
+
+}
+public Fraction pow(int n) {
+
+}
+public Fraction negate() {
+
+}
+@Override
+public String toString() {
+return numerator + "/" + denominator;
+}
+public Fraction(long num){
+numerator = num;
+denominator = 1;
+}
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+}
+}
